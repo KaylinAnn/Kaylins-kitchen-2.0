d20dc49c696b4a055870d84d766f9ffa70ecfaf9
KaylinAnn <40373229+KaylinAnn@users.noreply.github.com>
2019-05-13 16:40:03 -0700

    added more routes

--- src/Routes.js
+++ src/Routes.js
@@ -2,14 +2,17 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 import Home from './components/Home/Home';
 import Dashboard from './components/Dashboard/Dashboard'
+import Profile from './components/Profile/Profile'
+import Pantry from './components/Pantry/Pantry'
+import Recipe from './components/Recipe/Recipe'
 
 
 export default (
   <Switch>
     <Route path="/dashboard" component={Dashboard} />
-    {/* <Route path="/profile" component={Profile} />
+    <Route path="/profile" component={Profile} />
     <Route path="/recipe/:id" component={Recipe} />
-    <Route path="/pantry" component={Pantry} /> */}
+    <Route path="/pantry" component={Pantry} />
     <Route exact path="/" component={Home} />
   </Switch>
 );
